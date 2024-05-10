c07283e057281453b1590d7be5f6659efe168c06
Jack <jpick329@gmail.com>
2024-05-09 23:03:05 -0500

    init commit

---
+++ src/main/java/com/jackpickus/euchre/EuchreApplication.java
@@ -0,0 +1,13 @@
+package com.jackpickus.euchre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EuchreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EuchreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jackpickus/euchre/EuchreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jackpickus.euchre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EuchreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
